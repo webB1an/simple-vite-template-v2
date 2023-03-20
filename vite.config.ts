e817bea28b46aa7fb8d6c81e9e509814e3684ac3
webB1an <webB1anyaoyao@gmail.com>
2023-03-20 16:51:53 +0800

    build: change base path

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ import VueMacros from 'unplugin-vue-macros/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   resolve: {
     alias: {
       '~/': `${path.resolve(__dirname, 'src')}/`,
